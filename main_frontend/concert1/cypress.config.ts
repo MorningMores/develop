48647718775a441bbca75baaef5b031ce346ad82
Putinun-te <Putinun.te@gmail.com>
2025-10-08 15:44:00 +0700

    edit test unit

---
+++ main_frontend/concert1/cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/concert',
+    specPattern: 'cypress/e2e/**/*.cy.{ts,js}',
+    supportFile: 'cypress/support/e2e.ts',
+    video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 8000
+  }
+});
